9442e3c2bbcf8dfe600007ed31d93fbece00f090
devsyedhaider <devalihaider86@gmail.com>
2026-02-20 15:21:54 +0500

    done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Counter />
+    </>
+  );
+}
+
+function Counter() {
+  const [count, setCount] = useState(0);
+  const [step, setStep] = useState(1);
+
+  const date = new Date();
+  date.setDate(date.getDate() + count);
+
+  return (
+    <>
+      <div
+        style={{ display: "flex", justifyContent: "center", marginTop: "3rem" }}
+      >
+        <button onClick={(c) => setStep((c) => c - 1)}>-</button>
+        <span style={{ margin: ".5rem" }}> Count : {step} </span>
+        <button onClick={(c) => setStep((c) => c + 1)}>+</button>
+      </div>
+
+      <div
+        style={{ display: "flex", justifyContent: "center", marginTop: "3rem" }}
+      >
+        <button onClick={(c) => setCount((c) => c - step)}>-</button>
+        <span style={{ margin: ".5rem" }}> Count : {count} </span>
+        <button onClick={(c) => setCount((c) => c + step)}>+</button>
+      </div>
+
+      <p style={{ textAlign: "center" }}>
+        <span>
+          {count === 0
+            ? " Today is "
+            : count > 0
+              ? `${count} days from today is `
+              : `${Math.abs(count)} days ago was `}
+        </span>
+        <span>{date.toDateString()}</span>
+      </p>
+    </>
   );
 }
 
